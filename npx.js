ba29e05bd1e0bafd68768992efd02154e127b736
Ezekiel Aquino <ezekielaquino@users.noreply.github.com>
2021-09-01 15:21:18 +0200

    Edits package.json, adds npx bin script

---
+++ npx.js
@@ -0,0 +1,14 @@
+#! /usr/bin/env node
+
+'use strict';
+
+const appName = process.argv[2];
+const {spawnSync} = require('child_process');
+const url = 'git@github.com:ezekielaquino/starter3000.git';
+
+spawnSync('git', ['clone', url, `${process.cwd()}/${appName}`]);
+spawnSync('npm', ['install', '--prefix', `${process.cwd()}/${appName}`]);
+
+console.log('Bootstrapped a new project using Starter3000');
+console.log(`$ cd ${appName}`);
+console.log('$ yarn dev');
